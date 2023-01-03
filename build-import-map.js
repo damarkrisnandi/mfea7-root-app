8202f86406b4279cd35bd6a7df5987fdce07afc8
damar <damar@gitlab.lab>
2023-01-03 13:51:30 +0700

    move import map to public folder

--- build-import-map.js
+++ build-import-map.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 let obj = {
     table: []
 };
-fs.readFile('./public/assets/import-map.json', function readFileCallback(err, data) {
+fs.readFile('./public/import-map.json', function readFileCallback(err, data) {
     obj = JSON.parse(data);
     let json = JSON.stringify(obj, null, 2);
     fs.writeFileSync('./dist/import-map.json', json, 'utf-8');
