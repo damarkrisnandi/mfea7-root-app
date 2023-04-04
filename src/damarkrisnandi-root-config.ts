ae192826fb17d5ce5435960c82c43f5591be45d4
damar <damar@gitlab.lab>
2023-04-04 14:18:32 +0700

    test custom props

--- src/damarkrisnandi-root-config.ts
+++ src/damarkrisnandi-root-config.ts
@@ -4,9 +4,22 @@ import {
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
+import { HTMLLayoutData } from "single-spa-layout/dist/types/isomorphic/constructRoutes";
+const testProps = 'hello, this is test props';
 import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
+const layoutData: HTMLLayoutData = {
+  props: {
+    testProps
+  },
+  loaders: {
+    // mainContent: `<img src="loading.gif">`,
+    // // A single-spa parcel config
+    // topNav: singleSpaReact({...})
+  }
+};
+
+const routes = constructRoutes(microfrontendLayout, layoutData);
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
